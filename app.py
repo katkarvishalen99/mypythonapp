7939002ada44729f221011cd2833ea5f41ed9b50
vishal <katkarvishalen99@gmail.com>
2025-08-17 17:10:03 +0000

    add initial app files and kubernetes configs

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    hostname = socket.gethostname()
+    return f"Hello from Dockerized Python! Hostname: {hostname}"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
